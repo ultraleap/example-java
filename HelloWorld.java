9c2e932148f983a2cc4c5996441c59f38d5f9c68
James Heimbuck <jheimbuck@gitlab.com>
2020-01-03 21:22:43 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
         System.out.println("Hello, World");
     }
 
-    public void hello(int a, int b, int c, int d)
+    public void hello(int a, int b, int c, int d, int e, int f, int j, int k)
     {
         System.out.println("Here's a little function.");
     }
